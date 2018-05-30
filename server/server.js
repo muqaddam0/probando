e8fd8ba75380813055b0f18c486e55c2cbe7d26a
Rene Torres Alvarez <renetorres@Renes-MacBook-Air.local>
2018-05-30 10:12:20 -0500

    serv

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const { auth } = require('./middleware/auth')
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
+app.use(express.static('client/build'))
 
 
 
@@ -173,7 +173,12 @@ app.delete('/api/delete_book',(req,res)=>{
 })
 
 
-
+if(process.env.NODE_ENV=== 'production'){
+  const path = require('path');
+  app.get('/*',(req,res)=>{
+    res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+  })
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
