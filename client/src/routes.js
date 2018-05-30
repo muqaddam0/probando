7785f35832f75046dc1fae5d739c96630a3159c6
Rene Torres Alvarez <renetorres@Renes-MacBook-Air.local>
2018-05-30 10:45:07 -0500

    fix

--- client/src/routes.js
+++ client/src/routes.js
@@ -24,7 +24,7 @@ const Routes = () => {
       <Route path="/user" exact component={Auth(User,true)}/>
       <Route path="/user/add" exact component = {Auth(AddReview,true)}/>
       <Route path="/user/edit-post/:id" exact component = {Auth(EditBook,true)}/>
-      <Route path="/user/register" exact component = {Auth(Register,true)}/>
+      <Route path="/user/register" exact component = {Register}/>
       <Route path="/books/:id" exact component={Auth(BookView,null)}/>
       <Route path="/user/user-reviews" exact component = {Auth(UserPost,true)}/>
       </Switch>
